9747898b36c0ad27e9f1de951b8f8c85b3972ab7
iamtomek <12088991+thanaj@users.noreply.github.com>
2018-05-15 20:53:36 +0200

    Revert "environment for node set: babel, babelify,browserify,watchify"
    This reverts commit 15f28dcb1c2955a79dca66efedeaae51a10b8e9a.

--- index.js
+++
@@ -1,8 +0,0 @@
-let http = require('http');
-
-let server = http.createServer(function(req,res){
-  console.log('response for request');
-  res.end('<h1>Witaj świecie</h1>');
-})
-
-server.listen(3000);
